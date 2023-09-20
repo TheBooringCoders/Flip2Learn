960cefd952491e0fd9be3e0990c9b7a15dfb7449
Mir Sahib <mirsahib24@gmail.com>
2023-09-20 11:30:28 +0600

    :hammer: implement flutter tts

--- lib/board_page.dart
+++ lib/board_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flip2learn/colors.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class BoardPage extends StatefulWidget {
   const BoardPage({super.key});
@@ -8,32 +10,69 @@ class BoardPage extends StatefulWidget {
 }
 
 class _BoardPageState extends State<BoardPage> {
+  int currentIndex = 0;
+  final List<Map<String, String>> dict = [
+    {"Norsk": "norwegian"},
+    {"Jeg Heter": "My Name is"},
+    {"hallo": "hello"},
+    {"hvor mye": "how much"},
+    {"hvor er": "where is"},
+    {"jeg vil": "i would like"},
+    {"takk": "thank you"},
+  ];
+
+  void _speak() async {
+    FlutterTts flutterTts = FlutterTts();
+    Future.delayed(const Duration(milliseconds: 500));
+    await flutterTts.setLanguage('nb-NO');
+    await flutterTts.speak(dict[currentIndex].keys.first);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const SizedBox(height: 75),
+        const SizedBox(height: 25),
+        const Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Text('Header'),
+          Text('Current Score'),
+          Text('Total Score')
+        ]),
+        const SizedBox(height: 25),
         Card(
             margin: const EdgeInsets.only(left: 30, right: 30),
             child: Container(
-              //padding: const EdgeInsets.all(75),
               width: 400,
               height: 200,
               color: Colors.white,
-              child: const Center(
+              child: Center(
                   child: Text(
-                'Norsk',
+                dict[currentIndex].keys.first,
                 textScaleFactor: 2,
-                style: TextStyle(fontWeight: FontWeight.bold),
+                style: const TextStyle(fontWeight: FontWeight.bold),
               )),
             )),
-        const SizedBox(height: 75),
+        const SizedBox(height: 25),
+        IconButton(
+          onPressed: _speak,
+          icon: const Icon(Icons.volume_up),
+          color: primary,
+          iconSize: 32,
+          splashColor: primaryAccent,
+        ),
+        const SizedBox(height: 25),
         ElevatedButton(
             style: ButtonStyle(
               minimumSize: MaterialStateProperty.all(const Size(325, 50)),
             ),
             onPressed: () {
-              debugPrint('I Know this');
+              //debugPrint('I Know this');
+              setState(() {
+                currentIndex++;
+                if (currentIndex >= dict.length) {
+                  currentIndex = 0;
+                }
+              });
             },
             child: const Text('I Know this')),
         const SizedBox(height: 25),
@@ -42,7 +81,13 @@ class _BoardPageState extends State<BoardPage> {
               minimumSize: MaterialStateProperty.all(const Size(325, 50)),
             ),
             onPressed: () {
-              debugPrint('Don\'t know');
+              //debugPrint('Don\'t know');
+              setState(() {
+                currentIndex++;
+                if (currentIndex >= dict.length) {
+                  currentIndex = 0;
+                }
+              });
             },
             child: const Text('Don\'t know'))
       ],
