dc9cad3c9128e23f203b2b32bff6fb5dca17c678
Mir Sahib <mirsahib24@gmail.com>
2023-10-05 09:33:58 +0600

    :hammer: Step 1: Add app icon

--- lib/board_page.dart
+++ lib/board_page.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flip2learn/colors.dart';
 import 'package:flutter_tts/flutter_tts.dart';
@@ -12,6 +13,7 @@ class BoardPage extends StatefulWidget {
 class _BoardPageState extends State<BoardPage> {
   int currentIndex = 0;
   bool isBack = false;
+  double angle = 0;
   final List<Map<String, String>> dict = [
     {"Norsk": "norwegian"},
     {"Jeg Heter": "My Name is"},
@@ -29,8 +31,31 @@ class _BoardPageState extends State<BoardPage> {
     await flutterTts.speak(dict[currentIndex].keys.first);
   }
 
-  String getValue() {
+  String _getValue() {
     return dict[currentIndex].values.first;
+  }
+
+  // void _resetIsBack() {
+  //   Future.delayed(const Duration(seconds: 1), () {
+  //     setState(() {
+  //       isBack = false;
+  //     });
+  //   });
+  // }
+
+  void _flip() {
+    setState(() {
+      angle = (angle + pi) % (2 * pi);
+      if (angle >= pi / 2) {
+        isBack = true;
+        Future.delayed(const Duration(seconds: 2), () {
+          // Delayed reset of isBack after 2 seconds
+          setState(() {
+            isBack = false;
+          });
+        });
+      }
+    });
   }
 
   @override
@@ -44,31 +69,56 @@ class _BoardPageState extends State<BoardPage> {
           Text('Total Score')
         ]),
         const SizedBox(height: 25),
-        Card(
-          margin: const EdgeInsets.only(left: 30, right: 30),
-          child: isBack
-              ? Container(
-                  width: 400,
-                  height: 200,
-                  color: Colors.white,
-                  child: Center(
-                      child: Text(
-                    getValue(),
-                    textScaleFactor: 2,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  )),
-                )
-              : Container(
-                  width: 400,
-                  height: 200,
-                  color: Colors.white,
-                  child: Center(
-                      child: Text(
-                    dict[currentIndex].keys.first,
-                    textScaleFactor: 2,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  )),
-                ),
+        GestureDetector(
+          onTap: _flip,
+          child: TweenAnimationBuilder(
+              tween: Tween<double>(begin: 0, end: angle),
+              duration: const Duration(seconds: 1),
+              builder: (BuildContext context, double val, _) {
+                if (val >= pi / 2) {
+                  isBack = true;
+                } else {
+                  isBack = false;
+                }
+                return Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()
+                    ..setEntry(3, 2, 0.001)
+                    ..rotateY(val),
+                  child: Card(
+                    margin: const EdgeInsets.only(left: 30, right: 30),
+                    child: isBack
+                        ? Transform(
+                            alignment: Alignment.center,
+                            transform: Matrix4.identity()..rotateY(pi),
+                            child: Container(
+                              width: 400,
+                              height: 200,
+                              color: Colors.amber[300],
+                              child: Center(
+                                  child: Text(
+                                _getValue(),
+                                textScaleFactor: 2,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              )),
+                            ),
+                          )
+                        : Container(
+                            width: 400,
+                            height: 200,
+                            color: Colors.white,
+                            child: Center(
+                                child: Text(
+                              dict[currentIndex].keys.first,
+                              textScaleFactor: 2,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                            )),
+                          ),
+                  ),
+                );
+              }),
         ),
         const SizedBox(height: 25),
         IconButton(
